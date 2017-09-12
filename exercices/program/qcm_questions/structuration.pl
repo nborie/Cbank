e2e3e5411170b9c09f4c5b2e8aa12ba33e0da3e3
Nicolas Borie <nicolas.borie@u-pem.fr>
2017-09-13 00:13:24 +0200

    Une question de plus qcm programme

--- exercices/program/qcm_questions/structuration.pl
+++ exercices/program/qcm_questions/structuration.pl
@@ -9,6 +9,6 @@ answer2=Un programme C est une collection de fonctions qui s'articulent autour d
 answer3=Le langage C est un langage fonctionnel.
 answer4=Le langage C utilise principalement des balises ouvrantes et fermantes.
 answer5=L'indentation en C est très importante car on peut mettre des espaces, des tabulations et des retours à la ligne un peu partout.
-answer6=On ne peut pas faire de programme C sans commencer par inclure *<stdio.h>*
+answer6=On ne peut pas faire de programme C sans commencer par inclure <stdio.h>.
 right_answer1=Un programme C est une collection de fonctions qui s'articulent autour d'un main.
 right_answer2=L'indentation en C est très importante car on peut mettre des espaces, des tabulations et des retours à la ligne un peu partout.
