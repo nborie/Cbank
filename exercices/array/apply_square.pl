800355cf22a00269d385c1be9a5a784f9e7b40fa
Nicolas Borie <nicolas.borie@u-pem.fr>
2017-09-13 11:08:06 +0200

    Nouvel exercice sur les tableaux

---
+++ exercices/array/apply_square.pl
@@ -0,0 +1,72 @@
+# Copyright 2017 Nicolas Borie <nicolas.borie@u-pem.fr>
+author=Nicolas Borie
+name=Mettre au carré les éléments d'un tableau
+title=Mettre au carré les éléments d'un tableau
+tag=array
+template=/template/autograderC
+
+text==
+Écrire une fonction qui prend en argument un tableau d'entiers ainsi
+que ça taille puis élève aux carré tous les éléments contenus dans se
+tableau.
+==
+
+code==
+void apply_square_array(int* array, int nb_term){
+  /* votre code ici... */
+}
+==
+
+solution==
+void apply_square_array(int* array, int nb_term){
+  int i;
+
+  for (i=0 ; i<nb_term ; i++)
+    array[i] *= array[i];
+}
+==
+
+codeafter==
+#include <stdio.h>
+#include <stdlib.h>
+
+void display_array(int* array, int nb_term){
+  int i;
+
+  printf("[");
+  for (i=0 ; i<nb_term-1 ; i++)
+    printf("%d, ", array[i]);
+  printf("%d]\n", array[nb_term-1]);
+}
+
+int main(int argc, char* argv[]){
+  int nb_term = argc-1;
+  int tab = (int*)malloc(nb_term*sizeof(int));
+  int i;
+
+  for (i=0 ; i<nb_term ; i++){
+    tab[i] = atoi(argv[i+1]);
+  }
+
+  apply_square_array(tab, int nb_term);
+  display_array(tab, nb_term);
+  return 0;
+}
+==
+
+grader==
+from graderC import graderII
+import random
+
+tests = [["Exécution simple", "1", ""],
+         ["Quelques éléments", "12 -3 52 0 41", ""],
+         ["Tableau vide", "", ""]]
+
+chaine_tab_alea = ""
+for i in range(random.randint(5, 10)):
+    chaine_tab_alea += (" " + str(random.randint(-100, 100)))
+
+tests.append(["Aléatoire", chaine_tab_alea, ""])
+
+graderII(tests)
+==
