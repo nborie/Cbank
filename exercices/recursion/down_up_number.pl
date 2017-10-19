61118f85a177f8be49d1227af0fe59407dc94b19
Nicolas Borie <nicolas.borie@u-pem.fr>
2017-10-19 22:47:13 +0200

    Ajout d'un exercice sur la récursion

---
+++ exercices/recursion/down_up_number.pl
@@ -0,0 +1,70 @@
+# Copyright 2017 Nicolas Borie <nicolas.borie@u-pem.fr>
+#
+# Égrener des nombres récursivements
+
+author=Nicolas Borie
+name=Égrener des nombres récursivements
+title=Égrener des nombres récursivements
+tag=function|recursion
+template=/template/autograderC
+
+text==
+
+Écrire une fonction récurssive *bas_haut* qui prend en argument un
+entier *n* et qui affiche sur une seule ligne sans retour à la ligne
+tout les entiers de *n* à *1* puis remonte à *n*. Chaque monbre sera
+séparé par un espace.
+
+Pour *n* qui vaut *7*, cela donnera
+
+    7 6 5 4 3 2 1 2 3 4 5 6 7 
+    
+==
+
+code==
+... bas_haut(...){
+  ...
+}
+==
+
+solution==
+void bas_haut(int n){
+  if (n < 1)
+    return ;	
+  if (n == 1)
+    printf("1");
+  else{
+    printf("%d ", n);
+    bas_haut(n - 1);
+    printf(" %d", n);
+  }	
+}
+
+==
+
+codeafter==
+#include <stdio.h>
+#include <stdlib.h>    
+    
+int main(int argc, char* argv[]){
+  int a = atoi(argv[1]);
+
+  bas_haut(a);
+  return 0;
+}
+==
+
+    
+grader==
+from graderC import graderII
+import random
+    
+tests = [["Basique", "1", ""], 
+	 ["Moyen", "5", ""],
+	 ["Erreur", "-2", ""]]
+
+tests.append(["Aléatoire", str(random.randint(6, 15)), ""])
+tests.append(["Aléatoire", str(random.randint(6, 15)), ""])
+
+graderII(tests)
+==
