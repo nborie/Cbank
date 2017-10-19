68c9a48250792275fa1c2386e3af072acafc94eb
Nicolas Borie <nicolas.borie@u-pem.fr>
2017-10-19 21:40:40 +0200

    Plus de correction sur le dernier exercice

--- exercices/string/long_argument.pl
+++ exercices/string/long_argument.pl
@@ -11,8 +11,10 @@ template=/template/autograderC
 text==
 
 Écrire un programme C qui identifie son plus long argument et
-l'affiche ensuite en console encadré avec des caractères dièse. Le nom
-du programme fait parti des arguments.
+l'affiche ensuite en console encadré avec des caractères dièse. On
+considèra que le nom du programme (toujours en tête du tableau des
+arguments) ne fera pas parti des arguments. Sans argument, le
+programme ne devra rien afficher.
 
 Ainsi, si le plus long argument est le mot *pouet*, le programme devra alors afficher:
 
@@ -38,7 +40,10 @@ solution==
 int main(int argc, char* argv[]){
   int i, len_max=0, ind_max=0, current_len;
 
-  for (i=0 ; i<argc ; i++){
+  if (argc <= 1)
+    return 0;
+
+  for (i=1 ; i<argc ; i++){
     current_len = strlen(argv[i]);
     if (current_len > len_max){
       len_max = current_len;
