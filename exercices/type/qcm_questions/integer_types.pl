d4aa8f3f11a2709aad4c3021a7512acbb8100ba2
Nicolas Borie <nicolas.borie@u-pem.fr>
2017-11-16 12:15:31 +0100

    Premiere question sur la notion de type

---
+++ exercices/type/qcm_questions/integer_types.pl
@@ -0,0 +1,18 @@
+title=Les variables en C
+text==
+Parmi les types suivants, lesquels sont des types C entiers.
+==
+type=direct
+template=../plbank/gift/template/multiplechoices_template.pl
+answer1=int
+answer2=float
+answer3=unsigned char
+answer4=double
+answer5=long float
+answer6=short
+answer7=unsigned long int
+answer8=long double
+right_answer1=int
+right_answer2=unsigned char
+right_answer3=short
+right_answer4=unsigned long int
