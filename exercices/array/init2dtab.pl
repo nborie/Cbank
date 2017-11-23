12388e9cd7d6500e7536301ef85f3fcd03f21038
Nicolas Borie <nicolas.borie@u-pem.fr>
2017-11-23 11:49:39 +0100

    Quelques corrections 2 dernier exo tab2d

--- exercices/array/init2dtab.pl
+++ exercices/array/init2dtab.pl
@@ -30,36 +30,38 @@ Voici un exemple du contenu de la variable *tab* pour 11 lignes et 19 colonnes.
     i j k l m n o p q r s t u v w x y z a
     j k l m n o p q r s t u v w x y z a b
     k l m n o p q r s t u v w x y z a b c
-    
+
 ==
 
 code==
+
 ... init_tab_2d(char** tab, int n, int m){
   ...
 }
 ==
 
 solution==
+
 void init_tab_2d(char** tab, int n, int m){
   int i, j;
-  
+
   for(i=0 ; i<n ; i++){
     for(j=0 ; j<m ; j++){
       tab[i][j] = 'a' + (i+j % 26);
     }
-  }  
-}    
+  }
+}
 
 
 ==
-    
+
 codeafter==
 #include <stdlib.h>
-#include <stdio.h>    
+#include <stdio.h>
 
 void print_tab_2d(char** tab, int n, int m){
   int i, j;
-  
+
   for(i=0 ; i<n ; i++){
     for(j=0 ; j<m ; j++){
       printf("%c ", tab[i][j]);
@@ -67,7 +69,7 @@ void print_tab_2d(char** tab, int n, int m){
     printf("\n");
   }
 }
-    
+
 int main(int argc, char* argv[]){
   int n = atoi(argv[1]);
   int m = atoi(argv[2]);
@@ -76,20 +78,24 @@ int main(int argc, char* argv[]){
   char** tab = malloc(n*sizeof(char*));
   for (i=0 ; i<n ; i++)
     tab[i] = malloc(m*sizeof(char));
- 
+
   init_tab_2d(tab, n, m);
   print_tab_2d(tab, n, m);
-    
+
+  for (i=0 ; i<n ; i++)
+    free(tab[i]);
+  free(tab);
+
   return 0;
 }
 ==
 
-    
+
 grader==
 from graderC import graderII
 import random
-    
-tests = [["Basique", "1 1", ""], 
+
+tests = [["Basique", "1 1", ""],
 	 ["Vide", "0 0", ""],
 	 ["Ligne", "1 6", ""],
 	 ["Colonne", "7 1", ""],
