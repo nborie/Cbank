03318d7d17f82f92027e4d443f201cb9ef48c683
dr <dr@pl-test.u-pem.fr>
2018-10-12 22:11:57 +0200

    Fix max_of_three.pl

--- exercices/function/max_of_three.pl
+++ exercices/function/max_of_three.pl
@@ -6,17 +6,16 @@ author=NB&DR
 
 title=Maximum de trois entiers
 tag=function
-template=/template/autograderC
+template=/template/stdsandboxC.pl
 
 text==
 
-Écrire une fonction *max_of_three* qui prend trois entiers en
+Écrire une fonction **max_of_three** qui prend trois entiers en
 paramètres et retourne le maximum de ces trois entiers.
 
 ==
 
-code==
-
+editor.code==
 ... max_of_three(...){
   ...
 }
@@ -53,26 +52,15 @@ int main(int argc, char* argv[]){
 ==
 
 
-grader==
-from graderC import graderII
-import random
+tests==
 
-tests = [["Basique", "3 2 1", ""],
-	 ["Égaux", "10 10 10", ""],
-	 ["Négatif", "12 -6 3", ""]]
-
-tests.append(["Aléatoire", ' '.join([str(random.randint(-1000, 1000)) for i in range(3)]), ""])
-tests.append(["Aléatoire", ' '.join([str(random.randint(-1000, 1000)) for i in range(3)]), ""])
-tests.append(["Aléatoire", ' '.join([str(random.randint(-1000, 1000)) for i in range(3)]), ""])
-tests.append(["Aléatoire", ' '.join([str(random.randint(-1000, 1000)) for i in range(3)]), ""])
-
-graderII(tests)
-==
-
-feedback==
-attention pour obtenir des chiffres après la virgule il faut utiliser un double dans la division.
-
-  4/3 -> 1
-  4/3.0 -> 1.3333333
+[["Basique", "3 2 1", ""],
+["Égaux", "10 10 10", ""],
+["Négatif", "12 -6 3", ""],
+["Aléatoire", ' '.join([str(random.randint(-1000, 1000)) for i in range(3)]), ""],
+["Aléatoire", ' '.join([str(random.randint(-1000, 1000)) for i in range(3)]), ""],
+["Aléatoire", ' '.join([str(random.randint(-1000, 1000)) for i in range(3)]), ""],
+["Aléatoire", ' '.join([str(random.randint(-1000, 1000)) for i in range(3)]), ""]]
 
 ==
+
